1c0e20de30efd87c9bea7db285a2624ab0d4167a
Maxim Predko <maxpredko@gmail.com>
2022-06-16 20:14:30 +0300

    add routes / error page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import "./App.css";
 import { TodoAppBar } from "./components/AppBar/AppBar";
 import { ErrorSnackbar } from "./components/ErrorSnackbar/ErrorSnackbar";
 import { ToDoListContainer } from "./components/ToDoList/ToDoListContainer";
+import { LoginForm } from "./components/LoginForm/LoginForm";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Error404 from "./components/Error404/Error404";
 
 export const App = () => {
   return (
@@ -11,7 +14,12 @@ export const App = () => {
       <ErrorSnackbar />
       <TodoAppBar />
       <Container fixed>
-        <ToDoListContainer />
+        <Routes>
+          <Route path="/" element={<ToDoListContainer />} />
+          <Route path="login" element={<LoginForm />} />
+          <Route path="/404" element={<Error404 />} />
+          <Route path="*" element={<Navigate to="/404" />} />
+        </Routes>
       </Container>
     </div>
   );
