eb1aaf3ef9d99e95335c9c725d349a3f42c13a01
Maxim Predko <maxpredko@gmail.com>
2022-08-02 23:31:56 +0300

    refactor api.ts & create types.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,12 @@
 import axios, { AxiosResponse } from "axios";
-import { RequestStatusType } from "../store/reducers/app-reducer";
+import {
+  GetTasksResponse,
+  LoginParamsType,
+  ResponseType,
+  TaskType,
+  TodolistType,
+  UpdateTaskModelType,
+} from "./types";
 
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
@@ -11,19 +18,20 @@ export const instance = axios.create({
 
 export const authAPI = {
   authMe() {
-    return instance.get<ResponseType<AuthMeType>>("auth/me");
+    return instance.get<
+      ResponseType<{ id: number; email: string; login: string }>
+    >("auth/me");
   },
 
   login(data: LoginParamsType) {
     return instance.post<
       LoginParamsType,
-      AxiosResponse<ResponseType<{ userId: string }>>
+      AxiosResponse<ResponseType<{ userId?: string }>>
     >("auth/login", data);
   },
 
   logout() {
-    console.log("logout");
-    return instance.delete<LogoutResponseType>("auth/login");
+    return instance.delete<ResponseType<{ userId?: number }>>("auth/login");
   },
 };
 
@@ -33,126 +41,46 @@ export const todolistsAPI = {
   getTodolists() {
     return instance.get<TodolistType[]>("todo-lists");
   },
+
   createTodolist(title: string) {
     return instance.post<
       { title: string },
       AxiosResponse<ResponseType<{ item: TodolistType }>>
     >("todo-lists", { title });
   },
+
   deleteTodolist(id: string) {
     return instance.delete<ResponseType>(`todo-lists/${id}`);
   },
+
   updateTodolist(id: string, title: string) {
     return instance.put<{ title: string }, AxiosResponse<ResponseType>>(
       `todo-lists/${id}`,
       { title }
     );
   },
+
   getTasks(todolistId: string) {
     return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`);
   },
+
   deleteTask(todolistId: string, taskId: string) {
     return instance.delete<ResponseType>(
       `todo-lists/${todolistId}/tasks/${taskId}`
     );
   },
+
   createTask(todolistId: string, title: string) {
     return instance.post<
       { title: string },
       AxiosResponse<ResponseType<{ item: TaskType }>>
     >(`todo-lists/${todolistId}/tasks`, { title });
   },
+
   updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
     return instance.put<
       UpdateTaskModelType,
       AxiosResponse<ResponseType<{ item: TaskType }>>
     >(`todo-lists/${todolistId}/tasks/${taskId}`, model);
   },
-};
-
-// ==== TYPES ====
-
-export type AuthMeType = {
-  userId: number;
-  email: string;
-  login: string;
-};
-
-export type LoginResponseType = {
-  resultCode: number;
-  messages: string;
-  data: {
-    userId: number;
-  };
-};
-
-export type LoginParamsType = {
-  email: string;
-  password: string;
-  rememberMe?: boolean;
-  captcha?: boolean;
-};
-
-export type LogoutResponseType = {
-  resultCode: number;
-  messages: Array<string>;
-  // fieldsErrors?: Array<string>;
-  data: {};
-};
-
-export type TodolistType = {
-  id: string;
-  title: string;
-  addedDate: string;
-  order: number;
-};
-
-export type ResponseType<D = {}> = {
-  resultCode: number;
-  messages: Array<string>;
-  fieldsErrors: Array<string>;
-  data: D;
-};
-
-export enum TaskStatuses {
-  New = 0,
-  InProgress = 1,
-  Completed = 2,
-  Draft = 3,
-}
-
-export enum TaskPriorities {
-  Low = 0,
-  Middle = 1,
-  Hi = 2,
-  Urgently = 3,
-  Later = 4,
-}
-
-export type TaskType = {
-  description: string;
-  title: string;
-  status: TaskStatuses;
-  priority: TaskPriorities;
-  startDate: string;
-  deadline: string;
-  id: string;
-  todoListId: string;
-  order: number;
-  addedDate: string;
-  entityTaskStatus: RequestStatusType;
-};
-
-export type UpdateTaskModelType = {
-  title: string;
-  description: string;
-  status: TaskStatuses;
-  priority: TaskPriorities;
-  startDate: string;
-  deadline: string;
-};
-type GetTasksResponse = {
-  error: string | null;
-  totalCount: number;
-  items: TaskType[];
 };
